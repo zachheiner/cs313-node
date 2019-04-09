6b6583a0679f734734f1cb72ca5c90e4f3e7e07f
Zach Heiner <cardpunk1206@yahoo.com>
2019-04-09 02:09:54 -0600

    Change DB connection stuff for heroku.

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 let sequelize = null;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect : 'postgres',
     protocol: 'postgres'
 
